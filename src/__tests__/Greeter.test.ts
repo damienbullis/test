8e988adec03a4ee020a964717f7d338642af6dfe
Damien <damien@bodhi.digital>
2022-12-05 20:24:47 -0600

    Setup Package

---
+++ src/__tests__/Greeter.test.ts
@@ -0,0 +1,7 @@
+import { Greeter } from "../index";
+
+describe("Greeter", () => {
+  it("should greet with message", () => {
+    expect(Greeter("TypeScript")).toBe("Hello TypeScript");
+  });
+});
